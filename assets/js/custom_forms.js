773a0292bb134b1bf9b4513f12381a6efc0a6d13
Приданников Михаил <p.mihail174@yandex.ru>
2026-01-30 15:40:40 +0500

    обновил js

--- assets/js/custom_forms.js
+++ assets/js/custom_forms.js
@@ -1,11 +1,14 @@
 /**
  *
- * Custom Forms 4.1.1
+ * Custom Forms
  * Валидация форм
  *
- * Copyright 2025 Mihail Pridannikov
- *
- * Released on: July's 15, 2025
+ * @author      Mihail Pridannikov
+ * @copyright   2023-2026, Mihail Pridannikov
+ * @license MIT
+ * @version     4.1.1
+ * @release     July's 15, 2025
+ * @link        https://github.com/mihail-174/custom_forms
  *
  */
 
